074ce17dd09d6ef571f4e8c637c6ac400f934eb5
John Rae <john.rae23@gmail.com>
2015-10-29 13:24:17 -0400

    initial commit

---
+++ js/router.js
@@ -0,0 +1,32 @@
+import Backbone from 'backbone';
+import React from 'react';
+import ReactDom from 'react-dom';
+
+export default Backbone.Router.extend({
+
+  routes: {
+    ""       : "showHome",
+    "add"    : "showAdd",
+    "edit"   : "showEdit",
+    "detail" : "showDetail"
+
+  },
+
+  showHome() {
+
+  },
+
+  showDetail() {
+
+  },
+
+  showAdd() {
+
+  },
+
+  showEdit() {
+    
+  }
+
+
+});
